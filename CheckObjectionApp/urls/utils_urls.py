5e0b33d5585f7cdec0051a03f26281ff80b179e3
孤帆 <13680972988@163.coom>
2025-11-15 12:30:06 +0800

    重构utils工具相关url引用

--- CheckObjectionApp/urls/utils_urls.py
+++ CheckObjectionApp/urls/utils_urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
+from ..constants import URLNames
 from ..views.utils import batch_import_testcases ,image_code ,clear_my_submission_cache
 
 urlpatterns = [
-    path('batch-import-testcases/', batch_import_testcases, name='batch_import_testcases'),
+    path('batch-import-testcases/', batch_import_testcases, name=URLNames.BATCH_IMPORT_TESTCASES),
 # 生成验证码
     path('image_code',image_code,name='image_code'),
     # 删除缓存
